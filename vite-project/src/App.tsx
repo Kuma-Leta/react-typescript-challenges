c01baf46a9d8ada72caa0f882ccd0e897e63e900
kuma <kumaleta2021@gmail.com>
2024-05-02 10:25:33 +0300

    react routing added

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,18 +1,24 @@
 // import { useState } from 'react'
-import { BrowserRouter as Routes, Route, Router } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
-function App() {
+import ChallengeOne from "./components/challenge1";
+import ChallengeTwo from "./components/challenge2";
+import ChallengeThree from "./components/challenge3";
+import NotFound from "./components/notFound";
+const App: React.FC = () => {
   return (
-    <>
+    <div>
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
-
-          <Route />
+          <Route path="/chalenge1" element={<ChallengeOne />} />
+          <Route path="/chalenge2" element={<ChallengeTwo />} />
+          <Route path="/challenge3" element={<ChallengeThree />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </Router>
-    </>
+    </div>
   );
-}
+};
 
 export default App;
---
+++ vite-project/src/components/notFound.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+const NotFound: React.FC = () => {
+  return <div>Page Not Found</div>;
+};
+export default NotFound;
