92836226391c6f52f518cab5a11cd32a5566c23f
kuma <kumaleta2021@gmail.com>
2024-05-02 09:49:20 +0300

    first commit

---
+++ vite-project/src/App.tsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react'
+import { BrowserRouter as Routes, Route, Router } from "react-router-dom";
+function App() {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="" element />
+
+          <Route />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
