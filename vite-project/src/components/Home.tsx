3d9a1843133d1e7d4de88b588f6041895408f44c
kuma <kumaleta2021@gmail.com>
2024-05-03 10:29:54 +0300

    styling added

--- vite-project/src/components/Home.tsx
+++ vite-project/src/components/Home.tsx
@@ -1,17 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../styles/home.css";
 // import ChallengeOne from "./challenge1";
 const Home: React.FC = () => {
   return (
-    <>
-      <div className="text-blue underline">this is home page</div>
-      <div className="text-black">
+    <div className="home">
+      <div className="links">
         <Link to={"/challenge1"}>Challenge 1</Link>
         <Link to={"/challenge2"}>Challenge 2</Link>
         <Link to={"/challenge3"}>Challenge 3</Link>
         <Link to={"/challenge4"}>to do app</Link>
       </div>
-    </>
+      <div className="headersContainer">
+        <h1 className="">WellCome to List of Challenges Home Page</h1>
+        <h2> Please use the Above Links To Navigate Among The Challenges</h2>
+      </div>
+    </div>
   );
 };
 export default Home;
--- vite-project/src/components/challenge1.tsx
+++ vite-project/src/components/challenge1.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../styles/challenge1.css";
 const ChallengeOne: React.FC = () => {
   return (
-    <>
-      <div>Hello World !</div>
-      <Link to={"/"}>go Home page </Link>
-      <Link to={"/challenge2"}>Challenge 2</Link>
-      <Link to={"/challenge3"}>Challenge 3</Link>
-      <Link to={"/challenge4"}>to do app</Link>
-    </>
+    <div className="container">
+      <div className="linksContainer">
+        <Link to={"/"}>go Home page </Link>
+        <Link to={"/challenge2"}>Challenge 2</Link>
+        <Link to={"/challenge3"}>Challenge 3</Link>
+        <Link to={"/challenge4"}>to do app</Link>
+      </div>
+      <div className="hello">Hello World !</div>
+    </div>
   );
 };
 export default ChallengeOne;
